63fd361bd0acb18386d2ab1f718bdb432e0dced9
sherifemad53 <sherifemad53@gmail.com>
2026-01-20 10:34:17 +0200

    refactor: restrict CI/CD to main branch, and specify Kubernetes deployment image.

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,8 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+
+  map_public_ip_on_launch = true
 }
 
 # Create a security group
@@ -16,9 +18,23 @@ resource "aws_subnet" "main" {
 resource "aws_security_group" "main" {
   vpc_id = aws_vpc.main.id
   ingress {
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -30,44 +46,4 @@ resource "aws_instance" "main" {
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.main.id
   security_groups = [aws_security_group.main.name]
-}
-
-# Create a load balancer
-
-resource "aws_lb" "main" {
-  name               = "botit-load-balancer"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.main.id]
-  subnets            = [aws_subnet.main.id]
-}
-
-# Create a target group
-
-resource "aws_lb_target_group" "main" {
-  name     = "botit-target-group"
-  port     = 5000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-# Create a listener
-
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type = "forward"
-    forward_config {
-      target_group_arn = aws_lb_target_group.main.arn
-    }
-  }
-}
-
-# Create a target group attachment
-
-resource "aws_lb_target_group_attachment" "main" {
-  target_group_arn = aws_lb_target_group.main.arn
-  target_id        = aws_instance.main.id
 }
